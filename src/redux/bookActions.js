85ae847e4359a125b30cf76c7ce85cad3e77c93c
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-10-31 23:25:38 +0530

    Update bookActions.js

--- src/redux/bookActions.js
+++ src/redux/bookActions.js
@@ -1,21 +1,15 @@
-import {
-  fetchBooksRequest,
-  fetchBooksSuccess,
-  fetchBooksFailure
-} from './bookSlice';
+import { fetchBooksRequest, fetchBooksSuccess, fetchBooksFailure } from './bookSlice';
 
-const API_KEY = 'YOUR_NYT_BOOKS_API_KEY';
-const API_URL = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${GAteSicObGEiTD0QVm5EiTXNUe8NlB8A}`;
+const API_KEY = 'GAteSicObGEiTD0QVm5EiTXNUe8NlB8A';
+const API_URL = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${API_KEY}`;
 
-// Thunk to fetch books
 export const fetchBooks = () => async (dispatch) => {
   dispatch(fetchBooksRequest());
   try {
     const response = await fetch(API_URL);
-    if (!response.ok) throw new Error('Network response not ok');
+    if (!response.ok) throw new Error('Network response was not ok');
     const data = await response.json();
-    // Extract book list
-    const books = data.results.books.map((book) => ({
+    const books = data.results.books.map(book => ({
       title: book.title,
       author: book.author,
       publisher: book.publisher,
